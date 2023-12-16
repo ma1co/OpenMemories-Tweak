621aba95b8faccdd70a0e2212c53d8cd9c44f51f
Gordian Zhang <gordianzhang@yahoo.com>
2023-12-16 21:33:46 +0700

    Add clean hdmi button on video page

--- app/src/main/java/com/github/ma1co/openmemories/tweak/VideoActivity.java
+++ app/src/main/java/com/github/ma1co/openmemories/tweak/VideoActivity.java
@@ -1,5 +1,6 @@
 package com.github.ma1co.openmemories.tweak;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 public class VideoActivity extends ItemActivity {
@@ -8,5 +9,20 @@ public class VideoActivity extends ItemActivity {
         super.onCreate(savedInstanceState);
         addSwitch("Disable video recording limit", new NativeTweak(NativeTweak.Key.REC_LIMIT));
         addSwitch("Disable 4K video recording limit (RX100M4 only)", new NativeTweak(NativeTweak.Key.REC_LIMIT_4K));
+        addButton("Clean HDMI output", this::onHDMIButtonClicked);
+    }
+
+    private void onHDMIButtonClicked() {
+        Logger.info("onHDMIButtonClicked", "setting video codec to xavc to diable UI showing in HDMI");
+        try {
+            Shell.exec("bk.elf w 0x01070087 0a");
+        } catch (NativeException e) {
+            throw new RuntimeException(e);
+        }
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("HDMI clean output");
+        alert.setMessage("The video codec has now been set to XAVC to disable the recording function and prevent the UI showing in HDMI.\nChange it back to AVCHD to enable recording again.");
+        alert.setPositiveButton("Ok", null);
+        alert.show();
     }
 }
